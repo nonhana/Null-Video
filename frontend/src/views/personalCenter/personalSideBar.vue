f0717157e2b4fb62d1a7590b2103e81bc46dfa36
non_hana <1209220829@qq.com>
2023-11-01 11:15:14 +0800

    feat(dev): 引入图片裁剪插件

--- frontend/src/views/personalCenter/personalSideBar.vue
+++ frontend/src/views/personalCenter/personalSideBar.vue
@@ -14,6 +14,14 @@
           type="file"
           @change="fileSelected"
         />
+        <!-- 图片裁剪组件 -->
+        <imgCropper
+          :source-file="avatarSourceFile"
+          :cropped-file-type="avatarCroppedFileType"
+          :dialog-visible="avatarDialogVisible"
+          @upload-image="uploadImage"
+          @close-dialog="closeDialog"
+        />
         <div class="name">
           <span v-if="!nameInputVisable" @dblclick="showNameInput">{{
             userInfo!.user_name
@@ -142,6 +150,7 @@ import { useRoute, useRouter } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 import type { UserInfo } from '@/utils/types'
 import myInput from '@/components/form/input/input.vue'
+import imgCropper from '@/components/utils/imgCropper.vue'
 import arrowRight from '@/assets/svgs/arrow-right.svg'
 import myVideos from '@/assets/svgs/my-videos.svg'
 import myCollections from '@/assets/svgs/my-collections.svg'
@@ -149,6 +158,28 @@ import myFollowsAndFans from '@/assets/svgs/my-follows-and-fans.svg'
 import exit from '@/assets/svgs/exit.svg'
 import { useMessage, useDialog } from 'naive-ui'
 
+/* 头像图片相关 */
+const avatarDialogVisible = ref<boolean>(false)
+let avatarSourceFile: File | null | undefined = null
+let avatarCroppedFileType: string = '' // 裁剪后的文件类型
+// 点击更换头像时，打开文件选择器
+const openAvatarSelector = () => {
+  avatarSelector.value!.click()
+}
+// 选择文件后输出文件信息
+const fileSelected = () => {
+  const file = avatarSelector.value!.files![0]
+  console.log(file)
+  avatarSourceFile = file
+  avatarCroppedFileType = avatarSourceFile?.type ?? ''
+  avatarDialogVisible.value = true
+}
+const uploadImage = (value: { imgURL: string }) => {
+  userInfo.value!.user_avatar = value.imgURL
+}
+const closeDialog = () => {
+  avatarDialogVisible.value = false
+}
 /* Hooks */
 const message = useMessage()
 const dialog = useDialog()
@@ -167,7 +198,6 @@ const confirmExit = () => {
     }
   })
 }
-
 /* DOMs */
 const avatarSelector = ref<HTMLInputElement>()
 /* Routes */
@@ -221,15 +251,6 @@ const chooseItem = (index: number) => {
       confirmExit()
       break
   }
-}
-// 点击更换头像时，打开文件选择器
-const openAvatarSelector = () => {
-  avatarSelector.value!.click()
-}
-// 选择文件后输出文件信息
-const fileSelected = () => {
-  const file = avatarSelector.value!.files![0]
-  console.log(file)
 }
 // 双击用户名/失去焦点时，显示/隐藏用户名输入框
 const showNameInput = () => {
--- frontend/src/views/post/videoForm.vue
+++ frontend/src/views/post/videoForm.vue
@@ -18,8 +18,8 @@
       >
         <n-form-item path="cover" label="视频封面">
           <n-upload
-            action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f"
             list-type="image-card"
+            @before-upload="fileSelected"
             @preview="fileDetected"
           >
             点击上传
@@ -32,6 +32,14 @@
           >
             <img :src="previewImageUrl" style="width: 100%" />
           </n-modal>
+          <!-- 图片裁剪组件 -->
+          <imgCropper
+            :source-file="coverSourceFile"
+            :cropped-file-type="coverCroppedFileType"
+            :dialog-visible="coverDialogVisible"
+            @upload-image="uploadImage"
+            @close-dialog="closeDialog"
+          />
         </n-form-item>
         <n-form-item path="description" label="视频简介">
           <n-input
@@ -91,7 +99,26 @@
 import { ref } from 'vue'
 import { VIDEO_CATEGORY, VIDEO_TAG } from '@/utils/constants'
 import myButton from '@nullVideo/button/button.vue'
+import imgCropper from '@nullVideo/utils/imgCropper.vue'
 import { UploadFileInfo, FormRules } from 'naive-ui'
+
+/* 头像图片相关 */
+const coverDialogVisible = ref<boolean>(false)
+let coverSourceFile: File | null | undefined = null
+let coverCroppedFileType: string = '' // 裁剪后的文件类型
+// 选择文件后输出文件信息
+const fileSelected = (file: File) => {
+  coverSourceFile = file
+  coverCroppedFileType = coverSourceFile?.type ?? ''
+  coverDialogVisible.value = true
+}
+const uploadImage = (value: { imgURL: string }) => {
+  // userInfo.value!.user_cover = value.imgURL
+  videoForm.value.video_cover = value.imgURL
+}
+const closeDialog = () => {
+  coverDialogVisible.value = false
+}
 
 /* Props/Emits */
 const emits = defineEmits<{
